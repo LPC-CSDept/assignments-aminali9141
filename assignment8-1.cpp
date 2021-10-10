aa7b34c466babd1ec8603a4f2cef7030972622f5
aminali9141 <aminali9141@gmail.com>
2021-10-10 13:00:57 -0700

    resubmission

--- assignment8-1.cpp
+++ assignment8-1.cpp
@@ -10,37 +10,36 @@ int binarySearch(int [], int, int);
 int main()
 {
 	const int N = 10;
-	int numbers[N] = {10, 25, 30, 35, 40, 45, 55, 60, 65, 70 };
+	int number[N] = {10, 25, 30, 35, 40, 45, 55, 60, 65, 70 };
 	int target = 35;
 	int index;
 
 
-	index = binarySearch(numbers, N, target );
-	if ( index == -1)
-		cout << " not found \n";
-	else
-		cout << " foudn at index : " << index << endl;
+	index = binarySearch(number, N, target );
+	if ( index == -1) {
+		cout << target << " was not found" << endl;
+	} else {
+		cout << target << " was found" << endl;
+}
+	return 0;
 }
 
-int binarySearch(int number[], int N, int target)
-{
+int binarySearch(int number[], int N, int target) {
 	int first, last, mid;
-	first =0;
+	first = 0;
     last = N-1;
     
-    while (first<=last)
-
+    while (first <= last) {
     mid = (first+last)/2;
 
-    if (numbers[mid]==target)
-    cout<<"found at index; "<< mid <<endl;
+    if (number[mid] == target) {
+		return mid;
+	} else if (number[mid] > target) {
 
-    else if (numbers[mid]< target)
-    first = mid+1;
-    elselast = mid-1;
-    
-
-	return -1;
-
-
-}
+    last = mid - 1;;
+	} else {
+		first = mid + 1;
+	}
+  }
+  return -1;
+} 
