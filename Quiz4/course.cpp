696cbb4a8038b68c310985cc8f446beb74122d64
aminali9141 <aminali9141@gmail.com>
2021-11-23 11:21:12 -0800

    commit

--- Quiz4/course.cpp
+++ Quiz4/course.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 #include "course.hpp"
 
-Course:Course() {}
+static int NUM_COURSES = 0;
+Course::Course() {
+    NUM_COURSES++;
+}
 
 Course::Course(string cname, string credits, string semseter,vector<Student> students) {
     this->cname = cname;
     this->credits = credits;
     this->semester = semester;
     this->students = students; 
+}
+void Course::addStudent(Student &s) {
+    this->students.push_back(s);
+}
+
+void Course:::setCourse(string cname, int credits, string semester, vector<Student> students) {
+    this->cname = cname;
+    this->credits = credits;
+    this->semester = semester;
+    this->students = students;
+}
+
+int Course::getCountofCourses(){
+    return Course::NUM_COURSES;
+}
+
+istream& operator>> (istream& is, Course& course) {
+    is >>
+    return is;
 }
