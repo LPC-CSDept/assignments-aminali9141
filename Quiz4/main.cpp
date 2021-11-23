e8bbafb16303efd25f9799a10dca46d90fca9e2d
aminali9141 <aminali9141@gmail.com>
2021-11-23 12:59:18 -0800

    working on main

--- Quiz4/main.cpp
+++ Quiz4/main.cpp
@@ -6,16 +6,26 @@ using namespace std;
 
 int main(){
     Course course[4];
-    Course c;
+    Course c1;
+    Course c2;
+    Course c3;
+    Course c4;
     ifstream myfile("coursestudents.txt");
-    myfile>>c;
-    course[0] = c;
-    myfile>>c;
+    myfile>>course[0];
+    cout<<course[0];
+    cout << c;
+    cout<< "\n---------------------\n"
     course[1];
     myfile>>c;
+    cout << c;
+    cout<< "\n---------------------\n"
     course[2] = c;
     myfile>>c;
-    course[3] = c;
     cout << c;
+    cout<< "\n---------------------\n"
+    course[3] = c; 
+    cout << c;
+    cout << c;
+    cout<< "\n---------------------\n"
     return 0;
 }
