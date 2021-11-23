8e9ae75767e980cf65e852dab1a92ed8f92b18b7
aminali9141 <aminali9141@gmail.com>
2021-11-23 10:31:10 -0800

    just started to work on the quiz, had family issue

---
+++ Quiz4/student.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class Student {
+    private:
+    int id;
+    string sname;
+    char grade;
+    double scores;
+
+
+    public:
+      Student() {}
+      Student(int id, string sname, char grade, double scores) {
+          this->id = id;
+          this->name = name;
+          this->grade = grade;
+          this->scores = scores;
+      }
+
+}
