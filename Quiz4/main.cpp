4c996580eb85d0373b98746048206284bd54e738
aminali9141 <aminali9141@gmail.com>
2021-11-23 12:30:19 -0800

    commit

--- Quiz4/main.cpp
+++ Quiz4/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "course.hpp"
+#include <fstream>
+
 using namspace std;
+
 int main(){
+    Course course[4];
+    Course c;
+    ifstream myfile("coursestudents.txt");
+    myfile>>c;
+    course[0] = c;
+    myfile>>c;
+    course[1];
+    myfile>>c;
+    course[2] = c;
+    myfile>>c;
+    course[3] = c;
 
     return 0;
 }
