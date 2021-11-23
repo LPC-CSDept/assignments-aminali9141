b56b7a6b70027a006a6ed87e853d4e7899b23356
aminali9141 <aminali9141@gmail.com>
2021-11-23 11:52:39 -0800

    dealing with errors

--- Quiz4/course.hpp
+++ Quiz4/course.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "student.hpp"
+#include <string>
 
 using namespace std;
 class Course {
@@ -16,7 +17,7 @@ class Course {
 
       Course(string cname, int credits, string semester, vector<Student>students);
       
-      friend ostream& operator<< (ostream &os, const Course& n);
+      //friend ostream& operator<< (ostream &os, const Course& n);
       friend istream& operator<< (istream &is,Course& n);
 
       void addStudent(Student &s);
@@ -25,11 +26,30 @@ class Course {
 
       int getCountOfCourses();
 
-      }
+      string getCName();
+      int getCredit();
+      string getSemester();
+      vector<Student> getStudents();
+      
 };
 
-class Fraction{
-    //rest is the same
-};
+istream& operator<<(istream &is,const Course& course);
+{
+    string cname, semester;
+    int credits;
+    is>>cname>>credits>>semester;
+        int n, id;
+        double score;
+        string name;
+        char grade;
+        is>>n;
+        for(int i=0;i<n;i++){
+            is >> id >> name >> grade >> score;
+            Student stu(id, name, grade, score);
+            course.getStudents().push_back(stu);
+        }
+        course.setCourse(cname, credits, semester, course.getStudents())
+}
 
-ostream& operator<<(ostream &os, const Fraction& n)
+
+//ostream& operator<<(ostream &os, const Fraction& n)
