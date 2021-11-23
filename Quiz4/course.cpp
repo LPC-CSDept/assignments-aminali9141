5b138894b01b809ea0e5f78d2c645e4d2bcabba4
aminali9141 <aminali9141@gmail.com>
2021-11-23 12:19:29 -0800

    commit

--- Quiz4/course.cpp
+++ Quiz4/course.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include "course.hpp"
 
-static int NUM_COURSES = 0;
+using namespace std;
+
 Course::Course() {
-    NUM_COURSES++;
+    Course::NUM_COURSES++;
 }
 
-Course::Course(string cname, string credits, string semseter,vector<Student> students) {
+Course::Course(string cname, int credits, string semseter,vector<Student> students) {
     this->cname = cname;
     this->credits = credits;
     this->semester = semester;
-    this->students = students; 
+    this->students = students;
+    NUM_COURSES++;
 }
 void Course::addStudent(Student &s) {
     this->students.push_back(s);
@@ -25,4 +27,27 @@ void Course:::setCourse(string cname, int credits, string semester, vector<Stude
 
 int Course::getCountofCourses(){
     return Course::NUM_COURSES;
+}
+string Course::getCname(){
+    return this->cname;
+}
+int Course::getCredit(){
+    return this->credits;
+}
+string Course::getSemester(){
+    return this->semester;
+}
+vector<Student> Course::getStudents(){
+    return this->students;
+}
+
+istream& operator>>(istream &is, Course& course)
+{
+    string cname, semester;
+    int credits;
+    is >> cname>> credits>> semester;
+    Student stu(id, name, grade, score);
+    course.getStudents().push_back(stu);
+    course.setCourse(cname, credits, semester, course.getStudents());
+    return is;
 }
