df3b3c5d5933c287164acb3ee643b5b0fa69e68a
aminali9141 <aminali9141@gmail.com>
2021-11-29 08:29:20 -0800

    commit

---
+++ Midterm/Numbers.cpp
@@ -0,0 +1,21 @@
+#include "Numbers.hpp"
+
+Numbers::Numbers()
+{
+
+}
+
+Numbers::~Numbers()
+{
+
+}
+
+static int Numbers::getNumObjects(void)
+{
+
+}
+
+  int Numbers::operator >(const Numbers& n)
+  {
+      
+  }
