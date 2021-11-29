0b51f1605629718cd1e7e78c84add273818c6368
aminali9141 <aminali9141@gmail.com>
2021-11-29 08:43:44 -0800

    commit

--- Midterm/Numbers.cpp
+++ Midterm/Numbers.cpp
@@ -1,16 +1,20 @@
 #include "Numbers.hpp"
 
-Numbers::Numbers()
+	static int Numbers::NumofObjects=0;
+
+	Numbers::Numbers()
 	{
 		ID=0;
 	}
 
 	Numbers::Numbers(int i,int n)
 	{
+	  srand(time(0)); 
+
 		ID=i;
 		for (int i = 0; i < n; ++i)
 		{
-			values.push_back()
+			values.push_back(rand()%100);
 		}
 
 	}
@@ -23,12 +27,12 @@ Numbers::Numbers()
 
 	int Numbers::getID()
 	{
-
+		return ID;
 	}
 
 	static int Numbers::getNumObjects(void)
 	{
-
+		return NumofObjects;
 	}
 
     int Numbers::operator >(const Numbers& n)
@@ -38,3 +42,8 @@ Numbers::Numbers()
 	
 	void printNumbers(Numbers n)
 	{
+		for (int i = 0; i < n.size(); ++i)
+		{
+			cout<<n.values[i]<<endl;
+		}
+	}
--- Midterm/numbers.hpp
+++ Midterm/numbers.hpp
@@ -7,15 +7,18 @@ template <typename T>
 class Numbers
 {
 
-    private:
-      int ID;
-      vector<T> values;
-      static int NumofObjects;
+private:
+	int ID;
+	vector<T> values;
+	static int NumofObjects;
 
-    public:
-      Numbers();
-      ~Numbers();
-      static int getNumObjects(void);
-        int operator >(const Numbers& n);
-    friend  void printNumbers(Numbers n);
-}
+public:
+	Numbers();
+	Numbers(int i,int n);
+	~Numbers();
+	int getID();
+	static int getNumObjects(void);
+    int operator >(const Numbers& n);
+	friend  void printNumbers(Numbers n);
+	
+};
