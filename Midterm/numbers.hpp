5eb341dfb44417ba86dc4c192c15b30b37100804
aminali9141 <aminali9141@gmail.com>
2021-11-29 08:22:53 -0800

    starting

---
+++ Midterm/numbers.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namepspace std;
+
+template <typename T>
+class Numbers
+{
+
+    private:
+      int ID;
+
+    public:
+      Numbers();
+      ~Numbers();
+}
