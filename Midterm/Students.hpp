ce78943df77c9baa0e1f1c0b0de1b6d94c2fcf9a
aminali9141 <aminali9141@gmail.com>
2021-11-29 10:10:23 -0800

    had family troubles had to take a break

---
+++ Midterm/Students.hpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Students
+{
+private:
+	int ID;
+	string name;
+	vector<int> scores;
+	static int NumofObjects;
+
+public:
+	Students();
+	Students(int i,string n,int s1,int s2,int s3);
+
+	int getScore1();
+	int getScore2();
+	int getScore3();
+
+	static int getNumObjects(void);
+    int operator >(const Students& s);
+	friend void printStudent(Students n);
+	
+};
