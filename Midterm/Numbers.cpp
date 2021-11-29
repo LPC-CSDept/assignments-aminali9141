1a96aa84eb70fb56a7d9367d62638db9b9a2372e
aminali9141 <aminali9141@gmail.com>
2021-11-29 08:37:39 -0800

    commit

--- Midterm/Numbers.cpp
+++ Midterm/Numbers.cpp
@@ -1,21 +1,40 @@
 #include "Numbers.hpp"
 
 Numbers::Numbers()
-{
+	{
+		ID=0;
+	}
 
-}
+	Numbers::Numbers(int i,int n)
+	{
+		ID=i;
+		for (int i = 0; i < n; ++i)
+		{
+			values.push_back()
+		}
 
-Numbers::~Numbers()
-{
+	}
 
-}
 
-static int Numbers::getNumObjects(void)
-{
+	Numbers::~Numbers()
+	{
 
-}
+	}
 
-  int Numbers::operator >(const Numbers& n)
-  {
-      
-  }
+	int Numbers::getID()
+	{
+
+	}
+
+	static int Numbers::getNumObjects(void)
+	{
+
+	}
+
+    int Numbers::operator >(const Numbers& n)
+    {
+
+    }
+	
+	void printNumbers(Numbers n)
+	{
--- Midterm/numbers.hpp
+++ Midterm/numbers.hpp
@@ -9,8 +9,13 @@ class Numbers
 
     private:
       int ID;
+      vector<T> values;
+      static int NumofObjects;
 
     public:
       Numbers();
       ~Numbers();
+      static int getNumObjects(void);
+        int operator >(const Numbers& n);
+    friend  void printNumbers(Numbers n);
 }
