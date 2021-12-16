4ce2c08665074f815a232449547e5b2858e09986
aminali9141 <aminali9141@gmail.com>
2021-12-16 15:05:22 -0800

    reading file

--- Final/Q1/main.cpp
+++ Final/Q1/main.cpp
@@ -5,6 +5,22 @@
 using namespace std;
 
 int main() {
+  Course courses[10];
+  int count=10;
+  ifstream fin("courses.txt");
+  int ID;
+  string name;
+  int credit;
+
+  for (int i = 0; i < count; ++i)
+  {
+    fin>>ID;
+    fin>>name;
+    fin>>credit;
+    courses[i]=new Course(ID,name,credit);
+  }  
+  fin.close();
 
 
+  return 0;
 } 
